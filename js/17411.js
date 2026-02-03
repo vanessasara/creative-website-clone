f8f2b56d85ac8d5d032da99e3a518f5dc722b3ec
vanessasara <vanessasarah@gmail.com>
2026-02-03 16:19:23 +0500

    small changes

---
+++ js/17411.js
@@ -0,0 +1,1 @@
+let paths=window.location.pathname.split("/");{const t=document.createElement("link");t.rel="stylesheet",t.href="css/slater-49802.css",document.head.appendChild(t)}import("./slater-49655.js"),import("./slater-49467.js");
